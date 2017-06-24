d3c00f5c622da48df30f1935efdfda820147f74e
alexander-cohen <alxcoh@gmail.com>
2017-06-24 16:59:17 -0400

    added helper file, started new line algorithm

--- GraphicsEngine/drawer.c
+++ GraphicsEngine/drawer.c
@@ -5,6 +5,8 @@ int *get_buf (buffer_info *inf, int r, int c) {
 	return inf -> pixels + r * (inf -> width) + c;
 }
 
+//helper function, compares two slopes
+//returns -1 if first slope is less than second slope, 1 if greater, 0 if equal
 int compare_slope (int dx, int dy, int desired_dx, int desired_dy)
 {
 	int left = dy * desired_dx, right = desired_dy * dx;
@@ -24,6 +26,31 @@ int compare_slope (int dx, int dy, int desired_dx, int desired_dy)
 	}
 }
 
+
+int draw_line_octant1 (buffer_info *inf, const point p1, const point p2, int col, bool verbose)
+{
+	int dx = p2.x - p1.x;
+	int dy = p2.y = p1.y;
+
+	//ensures we are in octant 1
+	if (dx < 0 || dy < 0 || dy < dx)
+	{
+		return 1;
+	}
+
+	int curx = p1.x;
+	int cury = p1.y;
+
+	do
+	{
+		*get_buf (inf, cur_y, cur_x) = col;
+
+		
+
+	} while (cur_x != p2.x && cur_y != p2.y)
+
+}
+
 int draw_line (buffer_info *inf, const point p1, const point p2, int col, bool verbose)
 {
 	if (p1.x < 0 || p1.x >= (inf -> width) ||
@@ -31,7 +58,7 @@ int draw_line (buffer_info *inf, const point p1, const point p2, int col, bool v
 		p2.x < 0 || p2.x >= (inf -> width)|| 
 		p2.y < 0 || p2.y >= (inf -> height))
 	{
-		return -1;
+		return 1;
 	}
 	
 	if (verbose)
@@ -47,8 +74,6 @@ int draw_line (buffer_info *inf, const point p1, const point p2, int col, bool v
 
 	do
 	{
-		
-
 		*get_buf (inf, cur_y, cur_x) = col;
 
 		//delta x is either -1, 0, 1 and delta y is either -1, 0, 1
@@ -103,5 +128,10 @@ int draw_line (buffer_info *inf, const point p1, const point p2, int col, bool v
 		cur_y += dec_dy ? ydir : 0;
 
 	} while (cur_x != p2.x && cur_y != p2.y);
+}
+
+
+int draw_circle (buffer_info *inf, const point p, int rad, int col, bool verbose)
+{
 
 }
--- GraphicsEngine/drawer.h
+++ GraphicsEngine/drawer.h
@@ -18,5 +18,6 @@ typedef struct {
 int *get_buf (buffer_info *inf, int r, int c);
 
 int draw_line (buffer_info *inf, const point p1, const point p2, int col, bool verbose);
+int draw_circle (buffer_info *inf, const point p, int rad, int col, bool verbose);
 
 #endif
