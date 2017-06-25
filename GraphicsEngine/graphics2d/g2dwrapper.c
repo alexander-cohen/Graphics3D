8eae3d3763e02db1af39f3b7b02e39256b15fe23
alexander-cohen <alxcoh@gmail.com>
2017-06-24 22:35:42 -0400

    finished g2dwrapper (still untested)

--- GraphicsEngine/graphics2d/g2dwrapper.c
+++ GraphicsEngine/graphics2d/g2dwrapper.c
@@ -9,7 +9,7 @@ g2d_primitive *create_line (int x1, int y1, int x2, int y2, int thickness, int c
 }
 int encode_line (g2d_primitive *dst, int x1, int y1, int x2, int y2, int thickness, int col)
 {
-	dst -> type = LINE;
+	(dst -> type) = LINE;
 	memset (dst -> data, 0, sizeof (dst -> data));
 
 	(dst -> data)[0] = x1;
@@ -30,18 +30,137 @@ int draw_encoded_line (g2d_context *graphics_context, int *data)
 	return 0;
 }
 
-g2d_primitive *create_rect_outline (int x, int y, int width, int height, int thickness, int col);
-int encode_rect_outline (g2d_primitive *dst, int x, int y, int width, int height, int thickness, int col);
-int draw_encoded_rect_outline (g2d_context *graphics_context, int *data);
+g2d_primitive *create_rect_outline (int x, int y, int width, int height, int thickness, int col)
+{
+	g2d_primitive *rect_outline = (g2d_primitive *)malloc (sizeof (g2d_primitive));
+	encode_rect_outline (rect_outline, x, y, width, height, thickness, col);
+	return rect_outline;
+	
+}
+int encode_rect_outline (g2d_primitive *dst, int x, int y, int width, int height, int thickness, int col)
+{
+	(dst -> type) = RECT_OUTLINE;
+	memset (dst -> data, 0, sizeof (dst -> data));
 
-g2d_primitive *create_rect_fill (g2d_primitive *dst, int x, int y, int width, int height, int col);
-int encode_rect_fill (g2d_primitive *dst, int x, int y, int width, int height, int col);
-int draw_encoded_rect_fill (g2d_context *graphics_context, int *data);
+	(dst -> data)[0] = x;
+	(dst -> data)[1] = y;
+	(dst -> data)[2] = width;
+	(dst -> data)[3] = height;
+	(dst -> data)[4] = thickness;
+	(dst -> data)[5] = col;
 
-g2d_primitive *create_circle_outline (g2d_primitive *dst, int x, int y, int rad, int thickness, int col);
-int encode_circle_outline (g2d_primitive *dst, int x, int y, int rad, int thickness, int col);
-int draw_encoded_circle_outline (g2d_context *graphics_context, int *data);
+	return 0;
+}
+int draw_encoded_rect_outline (g2d_context *graphics_context, int *data)
+{
+	g2d_set_context (graphics_context);
+	g2d_set_col (data[5]);
+	g2d_draw_thick_rect (data[0], data[1], data[2], data[3], data[4]);
+}
 
-g2d_primitive *create_circle_fill (g2d_primitive *dst, int x, int y, int rad, int col);
-int encode_circle_fill (g2d_primitive *dst, int x, int y, int rad, int col);
-int draw_encoded_circle_fill (g2d_context *graphics_context, int *data);
+g2d_primitive *create_rect_fill (int x, int y, int width, int height, int col)
+{
+	g2d_primitive *rect_fill = (g2d_primitive *)malloc (sizeof (g2d_primitive));
+	encode_rect_fill (rect_fill, x, y, width, height, thickness, col);
+	return rect_fill;
+	
+}
+int encode_rect_fill (g2d_primitive *dst, int x, int y, int width, int height, int col)
+{
+	(dst -> type) = RECT_FILL;
+	memset (dst -> data, 0, sizeof (dst -> data));
+
+	(dst -> data)[0] = x;
+	(dst -> data)[1] = y;
+	(dst -> data)[2] = width;
+	(dst -> data)[3] = height;
+	(dst -> data)[4] = col;
+
+	return 0;
+}
+int draw_encoded_rect_fill (g2d_context *graphics_context, int *data)
+{
+	g2d_set_context (graphics_context);
+	g2d_set_col (data[4]);
+	g2d_fill_rect (data[0], data[1], data[2], data[3]);
+}
+
+g2d_primitive *create_circle_outline (int x, int y, int rad, int thickness, int col)
+{
+	g2d_primitive *circle_outline = (g2d_primitive *)malloc (sizeof (g2d_primitive));
+	encode_circle_outline (circle_outline, x, y, width, height, thickness, col);
+	return circle_outline;
+	
+}
+int encode_circle_outline (g2d_primitive *dst, int x, int y, int rad, int thickness, int col)
+{
+	(dst -> type) = CIRCLE_OUTLINE;
+	memset (dst -> data, 0, sizeof (dst -> data));
+
+	(dst -> data)[0] = x;
+	(dst -> data)[1] = y;
+	(dst -> data)[2] = rad;
+	(dst -> data)[3] = thickness;
+	(dst -> data)[4] = col;
+
+	return 0;
+}
+int draw_encoded_circle_outline (g2d_context *graphics_context, int *data)
+{
+	g2d_set_context (graphics_context);
+	g2d_set_col (data[4]);
+	g2d_draw_circle_outline (data[0], data[1], data[2], data[3]);
+}
+
+g2d_primitive *create_circle_fill (int x, int y, int rad, int thickness, int col)
+{
+	g2d_primitive *circle_fill = (g2d_primitive *)malloc (sizeof (g2d_primitive));
+	g2d_draw_thick_circle (circle_fill, x, y, width, height, thickness, col);
+	return circle_fill;
+	
+}
+int encode_circle_fill (g2d_primitive *dst, int x, int y, int rad, int thickness, int col)
+{
+	(dst -> type) = CIRCLE_FILL;
+	memset (dst -> data, 0, sizeof (dst -> data));
+
+	(dst -> data)[0] = x;
+	(dst -> data)[1] = y;
+	(dst -> data)[2] = rad;
+	(dst -> data)[3] = col;
+
+	return 0;
+}
+int draw_encoded_circle_fill (g2d_context *graphics_context, int *data)
+{
+	g2d_set_context (graphics_context);
+	g2d_set_col (data[3]);
+	g2d_fill_circle (data[0], data[1], data[2]);
+}
+
+int draw_primitive (g2d_context *graphics_context, g2d_primitive *primitive)
+{
+	switch (primitive -> type)
+	{
+		case LINE:
+			draw_encoded_line (graphics_context, primitive -> data);
+			break;
+		case RECT_OUTLINE:
+			draw_encoded_rect_outline (graphics_context, primitive -> data);
+			break;
+		case RECT_FILL:
+			draw_encoded_rect_fill (graphics_context, primitive -> data);
+			break;
+		case CIRCLE_OUTLINE:
+			draw_encoded_circle_outline (graphics_context, primitive -> data);
+			break;
+		case CIRCLE_FILL:
+			draw_encoded_circel_fill (graphics_context, primitive -> data);
+			break;
+		default:
+			printf ("did not find matching primitive.\n");
+			return 1;
+	}
+
+	return 0;
+}
--- GraphicsEngine/graphics2d/graphics2d.h
+++ GraphicsEngine/graphics2d/graphics2d.h
@@ -17,17 +17,18 @@ int g2d_set_context (g2d_context *buff);
 
 int g2d_fill_bg (int col);
 
-int g2d_draw_rect (int x, int y, int width, int height);
-int g2d_draw_thick_rect (int x, int y, int width, int height, int thickness);
-
-int g2d_fill_rect (int x, int y, int width, int height);
-
 int g2d_draw_point (int x, int y);
 
 int g2d_draw_line (int x1, int y1, int x2, int y2);
 int g2d_draw_thick_line (int x1, int y1, int x2, int y2, int thickness);
 
+int g2d_fill_rect (int x, int y, int width, int height);
+int g2d_draw_rect (int x, int y, int width, int height);
+int g2d_draw_thick_rect (int x, int y, int width, int height, int thickness);
+
+int g2d_fill_circle (int x, int y, int rad);
 int g2d_draw_circle (int x, int y, int rad);
+int g2d_draw_thick_circle (int x, int y, int rad, int thickness);
 
 
 #endif
