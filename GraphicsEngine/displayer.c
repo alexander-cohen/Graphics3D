c42e7651f460ecf72bbdde99b1379a5319800e47
Gabriel Marks <gmarks@stuy.edu>
2017-06-27 23:17:48 -0400

    :)

--- GraphicsEngine/displayer.c
+++ GraphicsEngine/displayer.c
@@ -442,14 +442,9 @@ int run_render_test() {
     XImage *img = NULL;
     gettimeofday(&begin, NULL);
 
-    matarrayvec materials = new_matarrayvec();
-    materials->append(materials, (material){CYAN});
-    materials->append(materials, (material){RED});
-    triarrayvec tris = new_triarrayvec();
     triangle t1 = (triangle){1,1,{0,0,300},{500,200,100},{300,500,100},{0,0,0},{0,0,0},{0,0,0},{0,0},{0,0},{0,0},0};
     triangle t2 = (triangle){1,1,{500,200,300},{0,500,0},{300,0,100},{0,0,0},{0,0,0},{0,0,0},{0,0},{0,0},{0,0},1};
-    tris->append(tris, t1);
-    tris->append(tris, t2);
+
     while(frameNum < 10000) {
         //g2d_fill_bg (CYAN);
 
@@ -464,13 +459,27 @@ int run_render_test() {
             }
         }
 
+        matarrayvec materials = new_matarrayvec();
+        materials->append(materials, (material){CYAN});
+        materials->append(materials, (material){RED});
+        triarrayvec tris = new_triarrayvec();
 
+        tris->append(tris, t1);
+        tris->append(tris, t2);
         col_arr = render(tris->data, 2, materials);
 
         img = XCreateImage(dis, CopyFromParent, 24, ZPixmap, 0, (char *)col_arr, WIDTH, HEIGHT, 32, 0);
         XPutImage(dis, win, gc, img, 0, 0, 0, 0, WIDTH, HEIGHT);
+        t1.p1.z += 1;
+        t1.p2.z -= 1;
+        t1.p3.z -= 1;
         t1.p1.x += 1;
-
+        t1.p2.y += 1;
+        t2.p1.z += 1;
+        t2.p2.z -= 1;
+        t2.p3.z += 1;
+        t2.p2.y -= 1;
+        t2.p1.x -= 1;
         printf("t1 p1 x: %f\n", t1.p1.x);
         free(col_arr);
         frameNum++;
