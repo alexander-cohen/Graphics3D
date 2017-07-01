593d46eea8249ed014618548cbf1eacde9fc9918
Gabriel Marks <gmarks@stuy.edu>
2017-07-01 00:57:37 -0400

    oops

--- GraphicsEngine/av2.h
+++ GraphicsEngine/av2.h
@@ -26,49 +26,4 @@ struct _AV_ {
 typedef struct _AV_* arrayvec;
 arrayvec new_arrayvec();
 
-void _AV__append(arrayvec st, void * dat) {
- if(st->len >= st->datalen) {
-  st->datalen *= 2;
-  st->data = realloc(st->data, st->datalen*sizeof(void *));
- }
- st->data[st->len] = dat;
- st->len++;
-}
-
-void * _AV__pop(arrayvec st) {
- assert(st->len);
- st->len--;
- return st->data[st->len];
-}
-
-void _AV__replace(arrayvec st, size_t i, void * dat) {
- st->data[i] = dat;
-}
-
-void _AV__replacemultiple(arrayvec st, size_t i, void * *dats, size_t lendats) {
- assert(lendats);
- if(lendats == 1) {
-  return _AV__replace(st, i, dats[0]);
- }
- _AV__replace(st, i, dats[0]);
- for(i = 1; i < lendats; i++) {
-  _AV__append(st, dats[i]);
- }
-}
-
-arrayvec new_arrayvec() {
- arrayvec res = malloc(sizeof(struct _AV_));
- res->len = 0;
- res->datalen = 1024;
- res->data = malloc(res->datalen*sizeof(void *));
- res->_append = _AV__append;
- res->_pop = _AV__pop;
- res->_replace = _AV__replace;
- res->_replace_many = _AV__replacemultiple;
-    return res;
-}
-
-void free_arrayvec(arrayvec st) {
- free(st->data);
-}
 #endif
